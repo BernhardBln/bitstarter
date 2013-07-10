f77b91606939c65b5a0dcb61eabed422751ab0d3
Bernhard Streit <Bernhard.Streit@gmail.com>
2013-07-10 16:20:52 +0000

    Funktioniert nun

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = null;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,8 +46,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(content, checksfile) {
+    $ = content;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +63,28 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var loadAndProcessContent = function(filename, url, callback){
+    if (url == null){
+	// load file
+	callback(cheerioHtmlFile(filename));
+}
+else{
+restler.get(url).on('complete', function (result){ callback(cheerio.load(result));});
+}
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html', null, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    loadAndProcessContent(program.file, program.url, function (content){
+	var checkJson = checkHtmlFile(content, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    });
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
